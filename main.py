74e91bd701cda874afe248a3f281ad2b332e7c38
aee4 <eeyramagbetor@gmail.com>
2024-09-04 19:43:25 +0000

    done

--- main.py
+++ main.py
@@ -3,38 +3,39 @@ from model import calculate_grade
 from model import calculate_weighted_gpa
 from model import semester_class
 
-program = input("Are there more students? Type yes to see results page")
-while program.lower() == "yes":
 
-  student_id = input("Enter your Student ID: ")
-  marks_programming = float(input("Enter your marks for Programming: "))
-  marks_hardware = float(input("Enter your marks for Hardware: "))
-  marks_aca_writing = float(input("Enter your marks for Academic Writing: "))
-  marks_french = float(input("Enter your marks for French: "))
+while True:
+    response = input("Do you want to calculate your semester results? (yes/no): ")
+    if response.lower() == "yes":
+        student_id = input("Enter your Student ID: ")
+        marks_programming = float(input("Enter your marks for Programming: "))
+        marks_hardware = float(input("Enter your marks for Hardware: "))
+        marks_aca_writing = float(input("Enter your marks for Academic Writing: "))
+        marks_french = float(input("Enter your marks for French: "))
 
-  credit_hours = 3
+        credit_hours = 3
 
-  grade_programming = calculate_grade(marks_programming)
-  grade_hardware = calculate_grade(marks_hardware)
-  grade_aca_writing = calculate_grade(marks_aca_writing)
-  grade_french = calculate_grade(marks_french)
+        grade_programming = calculate_grade(marks_programming)
+        grade_hardware = calculate_grade(marks_hardware)
+        grade_aca_writing = calculate_grade(marks_aca_writing)
+        grade_french = calculate_grade(marks_french)
 
-  gp_programming = calculate_grade_point(marks_programming)
-  gp_hardware = calculate_grade_point(marks_hardware)
-  gp_aca_writing = calculate_grade_point(marks_aca_writing)
-  gp_french = calculate_grade_point(marks_aca_writing)
+        gp_programming = calculate_grade_point(marks_programming)
+        gp_hardware = calculate_grade_point(marks_hardware)
+        gp_aca_writing = calculate_grade_point(marks_aca_writing)
+        gp_french = calculate_grade_point(marks_aca_writing)
 
-  wgp_programming = calculate_weighted_gpa(marks_programming)
-  wgp_hardware = calculate_weighted_gpa(marks_hardware)
-  wgp_aca_writing = calculate_weighted_gpa(marks_aca_writing)
-  wgp_french = calculate_weighted_gpa(marks_french)
+        wgp_programming = calculate_weighted_gpa(marks_programming)
+        wgp_hardware = calculate_weighted_gpa(marks_hardware)
+        wgp_aca_writing = calculate_weighted_gpa(marks_aca_writing)
+        wgp_french = calculate_weighted_gpa(marks_french)
 
-  total_credit_hours = credit_hours * 4
-  total_wgp = (wgp_programming + wgp_hardware + wgp_aca_writing + wgp_french)
-  sgpa = total_wgp / total_credit_hours
-  sem_class =semester_class(sgpa) 
+        total_credit_hours = credit_hours * 4
+        total_wgp = (wgp_programming + wgp_hardware + wgp_aca_writing + wgp_french)
+        sgpa = total_wgp / total_credit_hours
+        sem_class = semester_class(sgpa) 
 
-  print(f"""
+        print(f"""
 FIRST SEMESTER RESULTS *************************
 Student ID: {student_id}
 
@@ -54,4 +55,9 @@ Semester Class: {sem_class}
 
 ----------------------------------------END OF RESULTS --------------------------------------------
 """)
+    elif response.lower() == "no":
+        break
+    else:
+        print("Invalid response. Please enter 'yes' or 'no'.")
 
+  
